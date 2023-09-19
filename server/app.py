3633094aa29b78b8d87695b4e0322e0d6d8deddf
Ivan <arias.ivan@gmail.com>
2023-09-19 19:45:26 -0400

    lab done

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -17,6 +16,53 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict())
+        else:
+            return {"error": "User not found"}, 404
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+    
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        # Check if user_id exists in the session
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+
+            # Check if a user with that user_id exists in the database
+            if user:
+                return user.to_dict(), 200
+            else:
+                # This step is optional: It's not clearly defined in the given instructions, 
+                # but we're adding this for added security in case a user_id in session doesn't 
+                # match any user in the database.
+                return {'message': '401: Not Authorized'}, 401
+        
+        # If no user_id in session, return 401
+        return {'message': '401: Not Authorized'}, 401
+
+
+
+
+
+    
 
 class ClearSession(Resource):
 
@@ -47,7 +93,10 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
-
+    
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
